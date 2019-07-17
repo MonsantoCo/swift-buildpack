be6c8a248225d2ff34549e5725ee1ac5353b0fee
Vernon Schierding <schlingding@gmail.com>
2019-07-17 08:07:06 -0500

    Kitura NIO

--- bin/compile.sh
+++ bin/compile.sh
@@ -191,6 +191,7 @@ restore_cache
 cd $BUILD_DIR
 status "Fetching Swift packages and parsing Package.swift files..."
 if [ $(is_swift_version_greater_or_equal_to 4.0) == "true" ]; then
+  export KITURA_NIO=1
   swift package resolve | indent
 else
   swift package fetch | indent
